a958a21768f78e826805128cea695953731e98ab
Devansh Khullar <90106517+GeekyDevansh@users.noreply.github.com>
2022-01-29 15:33:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json({limit:"30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
 
 app.use(cors());
-app.use('/',postRoutes);
+app.use('/posts',postRoutes);
 
 
 
